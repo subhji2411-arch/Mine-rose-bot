ee119152c29f585ae49f338154c9577a59d03a62
subhji2411-arch <subhji2411@gmail.com>
2026-02-07 11:49:24 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,22 +1,14 @@
-from flask import Flask
 import threading
-import os
-
 from bot import main as bot_main
-
-app = Flask(__name__)
-
-@app.route("/")
-def home():
-    return "Telegram bot is running 🚀"
 
 def run_bot():
     bot_main()
 
 if __name__ == "__main__":
-    # Bot ko background thread me chalao
-    threading.Thread(target=run_bot).start()
+    t = threading.Thread(target=run_bot)
+    t.start()
 
-    # Render ka port bind
-    port = int(os.environ.get("PORT", 10000))
-    app.run(host="0.0.0.0", port=port)
+    # dummy loop so Render thinks service is alive
+    import time
+    while True:
+        time.sleep(60)
